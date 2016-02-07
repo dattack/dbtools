302e3675c60b90295ca8018e846aeec7e67d9114
cvarela <cvarela@dattack.com>
2016-02-07 18:45:07 +0100

    Fixed a bug using not cloneable configurations.
    Now, the internal configuration is a MapConfiguration instead of a
    CompositeConfiguration.

--- src/main/java/com/dattack/dbtools/drules/engine/ThreadContext.java
+++ src/main/java/com/dattack/dbtools/drules/engine/ThreadContext.java
@@ -15,8 +15,11 @@
  */
 package com.dattack.dbtools.drules.engine;
 
+import java.util.HashMap;
+
 import org.apache.commons.configuration.CompositeConfiguration;
 import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.MapConfiguration;
 import org.apache.commons.configuration.SystemConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,65 +32,77 @@ import com.dattack.ext.misc.ConfigurationUtil;
  */
 public final class ThreadContext {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(ThreadContext.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ThreadContext.class);
 
-	private static final ThreadLocal<ThreadContext> THREAD_LOCAL = new ThreadLocal<ThreadContext>();
+    private static final ThreadLocal<ThreadContext> THREAD_LOCAL = new ThreadLocal<ThreadContext>();
 
-	private CompositeConfiguration initialConfiguration;
+    private MapConfiguration internalConfiguration;
+    
+    private ThreadContext() {
+    }
 
-	private ThreadContext() {
-	}
+    /**
+     * Singleton method.
+     * 
+     * @return the singleton
+     */
+    public static synchronized ThreadContext getInstance() {
 
-	public synchronized static ThreadContext getInstance() {
+        ThreadContext obj = THREAD_LOCAL.get();
+        if (obj == null) {
+            obj = new ThreadContext();
+            THREAD_LOCAL.set(obj);
+        }
+        return obj;
+    }
 
-		ThreadContext obj = THREAD_LOCAL.get();
-		if (obj == null) {
-			obj = new ThreadContext();
-			THREAD_LOCAL.set(obj);
-		}
-		return obj;
-	}
+    private void setInitialConfiguration() {
+        setInitialConfiguration(null);
+    }
 
-	private void setInitialConfiguration() {
-		setInitialConfiguration(null);
-	}
+    /**
+     * Sets the initial configuration to use.
+     * 
+     * @param configuration
+     *            the initial configuration
+     */
+    public void setInitialConfiguration(final Configuration configuration) {
 
-	public void setInitialConfiguration(final Configuration configuration) {
+        if (internalConfiguration != null) {
+            LOGGER.warn("InitialConfiguration ");
+        }
 
-		if (initialConfiguration != null) {
-			LOGGER.warn("InitialConfiguration ");
-		}
+        CompositeConfiguration compositeConfiguration = new CompositeConfiguration();
+        compositeConfiguration.setDelimiterParsingDisabled(true);
+        compositeConfiguration.addConfiguration(new SystemConfiguration());
+        if (configuration != null) {
+            compositeConfiguration.addConfiguration(configuration);
+        }
+        internalConfiguration = new MapConfiguration(new HashMap<String, Object>());
+        internalConfiguration.copy(compositeConfiguration);
+    }
 
-		CompositeConfiguration compositeConfiguration = new CompositeConfiguration();
-		compositeConfiguration.setDelimiterParsingDisabled(true);
-		compositeConfiguration.addConfiguration(new SystemConfiguration());
-		if (configuration != null) {
-			compositeConfiguration.addConfiguration(configuration);
-		}
-		initialConfiguration = compositeConfiguration;
-	}
+    public Configuration getConfiguration() {
+        return getInternalConfiguration();
+    }
 
-	public Configuration getConfiguration() {
-		return getInternalConfiguration();
-	}
+    private MapConfiguration getInternalConfiguration() {
 
-	private CompositeConfiguration getInternalConfiguration() {
+        if (internalConfiguration == null) {
+            setInitialConfiguration();
+        }
+        return internalConfiguration;
+    }
 
-		if (initialConfiguration == null) {
-			setInitialConfiguration();
-		}
-		return initialConfiguration;
-	}
+    public void setProperty(final String key, final Object value) {
+        getInternalConfiguration().setProperty(key, value);
+    }
 
-	public void setProperty(final String key, final Object value) {
-		getConfiguration().setProperty(key, value);
-	}
+    public void clearProperty(final String key) {
+        getInternalConfiguration().clearProperty(key);
+    }
 
-	public void clearProperty(final String key) {
-		getConfiguration().clearProperty(key);
-	}
-
-	public String interpolate(final String value) {
-		return ConfigurationUtil.interpolate(value, getInternalConfiguration());
-	}
+    public String interpolate(final String value) {
+        return ConfigurationUtil.interpolate(value, getInternalConfiguration());
+    }
 }
